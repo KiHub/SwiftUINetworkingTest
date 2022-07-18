0419c9c3220dc82a5e3a409f281e52a01989d9c3
Kirill Ki <kirillkalapov@gmail.com>
2022-07-18 20:37:23 +0200

    Initial Commit

---
+++ SwiftUINetworking/SwiftUINetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINetworkingApp.swift
+//  SwiftUINetworking
+//
+//  Created by  Mr.Ki on 18.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
