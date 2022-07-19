6780d6d3063e48613a1fe30f89c6f14d7c5f264b
Kirill Ki <kirillkalapov@gmail.com>
2022-07-19 12:23:09 +0200

    Test

--- SwiftUINetworking/ContentView.swift
+++ SwiftUINetworking/ContentView.swift
@@ -8,10 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var posts: [Post] = []
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(posts) { post in
+                VStack {
+                    Text(post.title)
+                        .fontWeight(.bold)
+                    Text(post.body)
+                }
+            }.onAppear() {
+                NetworkManager().getPost { posts in
+                    self.posts = posts
+                }
+            }.navigationTitle("Posts")
+        }
+       //     .padding()
     }
+}
+
+struct Post: Codable, Identifiable {
+   // var id = UUID()
+    var id: Int
+    var title: String
+    var body: String
+}
+
+class NetworkManager {
+    func getPost(commpletion: @escaping ([Post]) -> ()) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            if let data = data {
+                let posts = try! JSONDecoder().decode([Post].self, from: data)
+                DispatchQueue.main.async {
+                    commpletion(posts)
+                }
+            }
+        }.resume()
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
